c3221ccd2397c1f514846a7dbeddebed97f5571e
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-04-06 19:45:55 +1200

    add api spec file

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+const yaml = require("js-yaml");
+const fs = require("fs");
+const swaggerUi = require("swagger-ui-express");
 const router = require("./router");
+
+const swaggerDocument = yaml.load(
+  fs.readFileSync(path.join(__dirname, "./apispec.yaml"), "utf8")
+);
 
 // middleware
 app.use(cors());
